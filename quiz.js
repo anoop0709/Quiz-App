a208f9aa5df40e822280bf98a2e013eaeec62fe5
Anoop <anoopsukumarannairk@gmail.com>
2022-04-15 00:04:21 +0100

    real questions and responsivness added

--- quiz.js
+++ quiz.js
@@ -7,27 +7,44 @@ var correct =0;
 let nxtBtn = document.querySelector(".nxtBtn");
 let prevBtn = document.querySelector(".prevBtn");
 let subBtn = document.querySelector(".subBtn");
-let questions = [
-  ["1.question 1","ans1","ans2","ans3","ans4","B"],
-  ["2.question 2","ans1","ans2","ans3","ans4","A"],
-  ["3.question 3","ans1","ans2","ans3","ans4","C"],
-  ["4.question 4","ans1","ans2","ans3","ans4","D"],
-  ["5.question 5","ans1","ans2","ans3","ans4","A"],
-  ["6.question 6","ans1","ans2","ans3","ans4","C"],
-  ["7.question 7","ans1","ans2","ans3","ans4","B"],
-  ["8.question 8","ans1","ans2","ans3","ans4","A"],
-  ["9.question 9","ans1","ans2","ans3","ans4","D"],
-  ["10.question 10","ans1","ans2","ans3","ans4","B"],
-  ["11.question 11","ans1","ans2","ans3","ans4","C"],
-  ["12.question 12","ans1","ans2","ans3","ans4","A"]
-];
-
+let questionsArray =[
+  ['What is the capital of Finland?','Helsinki','Rovaniemi','Tampere','Saariselka','A'],
+  ['What’s a baby rabbit called?','Coneys','Bunnies','Kittens','Doe','C'],
+  ['What fruit takes the scientific name Mangifera indica?','Apple','Kiwi','Orange','Mango','D'],
+  ['How many bones does a shark have?',360,192,36,'None','D'],
+  ['What is the smallest country in the world?','Cuba','Vatican','Nauru','Monaco','B'],
+  ['How many time zones are there in Russia?',2,11,4,10,'B'],
+  ['Which UK city is Banksy from?','Bristol','Liverpool','London','Swansea','A'],
+  ['What kind of food is Penne?','Salad','Sandwich','Noodles','Pasta','D'],
+  ['How many permanent teeth does a dog have?',35,42,28,16,'B'],
+  ['Which country in the world is believed to have the most miles of motorway?','India','America','China','Russia','C'],
+  ['What language is spoken in Brazil?','Brazilian','German','Portuguese','Indigenous','C'],
+  ['How many keys are there on a piano?',88,46,64,56,'A']
+  ];
+  
 
 
 function renderQuestion(){
-  if(pos >= questions.length){
-    divContainer.innerHTML = "<h1>Test Finished</h1>";
-    divContainer.innerHTML += "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+  if(pos >= questionsArray.length){
+    
+    if(correct == questionsArray.length){
+      divContainer.innerHTML = "<h2>Test Finished</h2>";
+      divContainer.innerHTML += "<h4>You got "+correct+" of "+questionsArray.length+" questions correct</h4>";
+      divContainer.innerHTML = "<h2>AWESOME!</h2>";
+    }else if(correct >= 9 && correct <= 11){
+      divContainer.innerHTML = "<h2>Test Finished</h2>";
+      divContainer.innerHTML += "<h4>You got "+correct+" of "+questionsArray.length+" questions correct</h4>";
+      divContainer.innerHTML += "<h2>GOOD!</h2>";
+    }else if(correct >= 5 && correct <= 8){
+      divContainer.innerHTML = "<h2>Test Finished</h2>";
+      divContainer.innerHTML += "<h4>You got "+correct+" of "+questionsArray.length+" questions correct</h4>";
+      divContainer.innerHTML += "<h2>AVERAGE</h2>";
+    }else{
+      divContainer.innerHTML = "<h2>Test Finished</h2>";
+      divContainer.innerHTML += "<h4>You got "+correct+" of "+questionsArray.length+" questions correct</h4>";
+      divContainer.innerHTML += "<h2>PLEASE TRY AGAIN</h2>";
+      
+    }
     pos = 0;
     correct = 0;
     return false;
@@ -35,12 +52,12 @@ function renderQuestion(){
 
 
   counter.innerHTML = pos+1;
-  question = questions[pos][0];
-  charA = questions[pos][1];
-  charB = questions[pos][2];
-  charC = questions[pos][3];
-  charD = questions[pos][4];
-  if(pos == questions.length-1){
+  question = questionsArray[pos][0];
+  charA = questionsArray[pos][1];
+  charB = questionsArray[pos][2];
+  charC = questionsArray[pos][3];
+  charD = questionsArray[pos][4];
+  if(pos == questionsArray.length-1){
     divContainer.innerHTML = "<h3>"+question+"</h3>";
     divContainer.innerHTML += "<input type = 'radio' name = 'choices' value = 'A'>"+charA+"<br>";
     divContainer.innerHTML += "<input type = 'radio' name = 'choices' value = 'B'>"+charB+"<br>";
@@ -72,7 +89,7 @@ function checkAnswer(){
   for(var i=0; i<choices.length; i++){
     if(choices[i].checked == true){
       choice = choices[i].value;
-      if(choice == questions[pos][5]){
+      if(choice == questionsArray[pos][5]){
         correct++;
       }
     }
